db70feef8e480fa9197e0b157ca715a8360f9a38
Diogo Galdino Stihler <diogo.galdino@philips.com>
2021-06-21 23:27:59 -0300

    [ReactJS] - add style home auth google

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,18 @@ import illustration from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIcon from '../assets/images/google-icon.svg';
 
+import './../styles/auth.scss';
+
 export function Home() {
   return (
-    <div>
+    <div id="page-aut">
       <aside>
         <img src={illustration} alt="Ilustração" />
-
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
-      <main>
-        <div>
+      <main >
+        <div className="main-content">
           <img src={logoImg} alt="Logo let me ask" />
           <button>
             <img src={googleIcon} alt="Logo google" />
