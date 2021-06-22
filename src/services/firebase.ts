e156a78ed0ed1b25f9c8bf668f32de1f9f7c7c61
Diogo Galdino Stihler <diogo.galdino@philips.com>
2021-06-21 22:17:48 -0300

    [ReactJS] - config firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+
+import 'firebase/auth';
+import 'firebase/database';
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DAMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJETCT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGIN_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const dataBase = firebase.database();
